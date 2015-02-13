7368111942dcb8faf1371f45cf9a8764820ecc0f
Hirofumi Wakasugi <baenej@gmail.com>
2015-02-13 17:28:29 +0900

    Add some options

--- default_template.rb
+++ default_template.rb
@@ -16,11 +16,23 @@ gem_group :development, :test do
   gem 'pry-rails'
   gem 'pry-stack_explorer'
   gem 'quiet_assets'
-  gem 'rack-mini-profiler'
+  gem 'rack-mini-profiler', require: false
   gem 'rails_best_practices'
   gem 'rails-footnotes'
   gem 'tapp'
 end
+
+initializer 'rack_profiler.rb', <<-CODE
+  if Rails.env == 'development'
+    require 'rack-mini-profiler'
+  
+    # initialization is skipped so trigger it
+    Rack::MiniProfilerRails.initialize!(Rails.application)
+  
+    # Display on right
+    Rack::MiniProfiler.config.position = 'right'
+  end
+CODE
 
 gem_group :test do
   gem 'minitest-rails'
@@ -34,7 +46,7 @@ end
 
 if yes?('Do you want to use rails assets?')
   add_source('https://rails-assets.org')
-  gem 'rails-assets-bootstrap' if yes?('Do you want to use bootstrap?')
+  gem 'rails-assets-bootstrap' if yes?('Do you want to use rails-assets-bootstrap?')
 end
 
 if yes?('Do you want to use sorcery?')
@@ -43,6 +55,12 @@ end
 
 if yes?('Do you want to use kaminari?')
   gem 'kaminari'
+end
+
+if yes?('Do you want to use bootstrap?')
+  gem 'bootstrap-sass'
+  remove_file './app/assets/stylesheets/application.css'
+  get 'https://raw.githubusercontent.com/5t111111/rails-templates/master/application-bootstrap.css.scss', './app/assets/stylesheets/application.css.scss'
 end
 
 application <<-GENERATORS
@@ -57,6 +75,7 @@ get 'https://raw.githubusercontent.com/5t111111/rails-templates/master/applicati
 run 'bundle install'
 
 after_bundle do
+  generate('rails_footnotes:install')
   git :init
   git add: '.'
   git commit: "-a -m 'Initial commit'"
